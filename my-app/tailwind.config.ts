df9f3c470337cb96d06f51a7a3599877fb6e22a2
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2024-12-15 21:16:44 +0200

    Color system setup

--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -1,18 +1,32 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: "var(--color-primary)",
+				secondary: "var(--color-secondary)",
+
+				success: "var(--color-success)",
+				error: "var(--color-error)",
+				warning: "var(--color-warning)",
+				info: "var(--color-info)",
+
+				bgPrimary: "var(--bg-primary)",
+				bgSecondary: "var(--bg-secondary)",
+
+				textPrimary: "var(--text-primary)",
+				textSecondary: "var(--text-secondary)",
+
+				hoverPrimary: "var(--hover-primary)",
+				hoverSecondary: "var(--hover-secondary)",
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
