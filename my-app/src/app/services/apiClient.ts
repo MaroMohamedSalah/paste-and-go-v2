e7da17ae6e878f3ab19da278fddbf1278adad038
Marwan.Mohamed <Marwan.Mohamed@consultsagacity.com>
2025-01-07 17:29:23 +0200

    Cache the result

--- my-app/src/app/services/apiClient.ts
+++ my-app/src/app/services/apiClient.ts
@@ -2,6 +2,7 @@ export const sendGetRequest = async (url: string, options: any) => {
 	return fetch(url, {
 		...options,
 		method: "GET",
+		cache: "force-cache",
 	}).then((res) => {
 		if (!res.ok) {
 			throw new Error(`HTTP error: ${res.status}`);
