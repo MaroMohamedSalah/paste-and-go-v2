c325eaedfd1357e6efe1d164ad1fc0a600bea8fe
Marwan.Mohamed <Marwan.Mohamed@consultsagacity.com>
2025-01-20 23:39:33 +0200

    Update IG endpoint

--- my-app/src/app/config/endpoints.ts
+++ my-app/src/app/config/endpoints.ts
@@ -17,6 +17,12 @@ export const ENDPOINTS = {
 	},
 	ig_user_info: (user_or_url: string) =>
 		`https://instagram-scraper-api2.p.rapidapi.com/v1/info?username_or_id_or_url=${user_or_url}`,
-	ig_reel: (reelUrl: string) =>
-		`https://instagram-api41.p.rapidapi.com/url?url=${reelUrl}`,
+	ig_reel: {
+		url: (reelUrl: string) =>
+			`https://instagram-reels-downloader-api.p.rapidapi.com/download?url=${reelUrl}`,
+		headers: {
+			"x-rapidapi-key": "251033cf4emshf3054a8b4557678p1fac0ajsn81864a3c4d44",
+			"x-rapidapi-host": "instagram-reels-downloader-api.p.rapidapi.com",
+		},
+	},
 };
